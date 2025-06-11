55858d5e2e82cd72cbd187b668e31178a1e57922
noahmasterson-art <noah.masterson@gmail.com>
2025-06-11 13:08:52 -0500

    Updates to single-page setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
 module.exports = function (eleventyConfig) {
+  // Copy static assets to output
   eleventyConfig.addPassthroughCopy("assets");
 
-  eleventyConfig.addCollection("pages", function (collectionApi) {
-    return collectionApi.getFilteredByTag("pages").reverse();
-  });
-
   return {
+    dir: {
+      input: ".",           // your source folder
+      includes: "_includes",// layout/partials folder
+      output: "docs"        // GitHub Pages-ready folder
+    },
     templateFormats: ["html", "njk", "md"],
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
